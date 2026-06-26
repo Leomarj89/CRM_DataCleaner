d28e44fe2f5858420f2804c71530fcc015680832
Leomar Contreras <leomar.2612.15@gmail.com>
2026-06-26 06:26:15 -0400

    Primer commit

---
+++ config.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+ROOT = Path(__file__).parent
+
+CARPETA_ENTRADA = ROOT / "entrada"
+
+CARPETA_SALIDA = ROOT / "salida 25.06.2026"
+
+CARPETA_LOGS = ROOT / "logs"
+
+CARPETA_DATOS = ROOT / "datos"
+
+NOMBRE_PROYECTO = "CRM Data Cleaner"
+
+VERSION = "1.0.0"
---
+++ src/utilidades.py
@@ -0,0 +1,11 @@
+crear_carpeta()
+
+quitar_tildes()
+
+normalizar_texto()
+
+limpiar_espacios()
+
+obtener_fecha()
+
+etc
